bb9afd4efed7472314a2b34b738506293d60f92b
fangjy <fangjingyu0502@163.com>
2020-12-14 16:31:30 +0800

    提交

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
       {{ getUsername }},欢迎来到Home
+      <p>{{total}}</p> 
   </div>
 </template>
 
 <script>
+import { mapState, } from 'vuex'
   export default {
     name:'Home',
     computed: {
+      ...mapState('order', ['total']),
       getUsername() {
         return this.$store.state.username;
       }
